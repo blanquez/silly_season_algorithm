803ed27f09947eb788d8e10fc106515cf733f9f1
blanquez <ablanquezp@gmail.com>
2020-07-25 21:26:53 +0200

    v1.1

--- makefile
+++
@@ -1,7 +0,0 @@
-all: compilar
-
-compilar: silly_season.cpp
-	g++ -O2 -o silly_season silly_season.cpp
-
-clean: 
-	rm silly_season
--- silly_season.sh
+++ silly_season.sh
@@ -3,11 +3,11 @@ declare -a seeds=('798245613' '123456789' '25022020' '17042026' '459268694')
 for i in "${seeds[@]}"
 do
 	echo -e "\n$i:\n"
-	./silly_season iris_set.dat iris_set_const_10.const 3 $i
-	./silly_season newthyroid_set.dat newthyroid_set_const_10.const 3 $i
-	./silly_season rand_set.dat rand_set_const_10.const 3 $i
+	./code/silly_season iris_set.dat iris_set_const_10.const 3 $i
+	./code/silly_season newthyroid_set.dat newthyroid_set_const_10.const 3 $i
+	./code/silly_season rand_set.dat rand_set_const_10.const 3 $i
 	echo
-	./silly_season iris_set.dat iris_set_const_20.const 3 $i
-	./silly_season newthyroid_set.dat newthyroid_set_const_20.const 3 $i
-	./silly_season rand_set.dat rand_set_const_20.const 3 $i     
+	./code/silly_season iris_set.dat iris_set_const_20.const 3 $i
+	./code/silly_season newthyroid_set.dat newthyroid_set_const_20.const 3 $i
+	./code/silly_season rand_set.dat rand_set_const_20.const 3 $i     
 done
